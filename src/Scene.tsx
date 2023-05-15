e68e97c3b42d5c34f4f4a7139a30d98cbe169574
tadazly <tadazly@163.com>
2023-05-15 13:30:42 +0800

    add some controls

--- src/Scene.tsx
+++ src/Scene.tsx
@@ -7,24 +7,32 @@ import Floor from './components/physic/cannon/floor'
 import Jelly from './components/physic/cannon/jelly'
 import RagdollScene from './components/ragdoll/ragdollScene'
 import { useControls, button } from 'leva'
-import { EffectComposer, Bloom } from '@react-three/postprocessing'
+import { EffectComposer, Bloom, DepthOfField } from '@react-three/postprocessing'
 import CrystallBall from './components/mesh/crystalBall'
 import Bunny from './components/mesh/bunny'
 
 export default function Scene() {
     const { idArray, enableOrbitControls, add } = useStore()
-    const { gravity } = useControls({
-        gravity: { value: [0, -9.8], step: 1, joystick: "invertY" }
+    const { gravity, autoRotate } = useControls({
+        gravity: { value: [0, -9.8], step: 1, joystick: "invertY" },
+        autoRotate: {value: true}
     })
     const { jellyScale } = useControls("Jelly", {
         jellyScale: { value: 2, min: 0.1, max: 10, step: 0.1 },
         addJelly: button(() => add(1)),
         addJelly30: button(() => add(30)),
     })
+    const { enabled: DOFswitch, focusDistance, focalLength, bokehScale, height } = useControls("DepthOfField", {
+        enabled: {value: true},
+        focusDistance: {value: 0.62, min: 0, max: 1, step: 0.001},
+        focalLength: {value: 0.2, min: 0, max: 1, step: 0.001},
+        bokehScale: {value: 3, min: 1, max: 5, step: 0.001},
+        height: {value: 600, min: 1, max: 1000, step: 1},
+    })
 
     return <>
         {/* <Perf position="top-left" /> */}
-        <OrbitControls makeDefault enabled={enableOrbitControls} autoRotate autoRotateSpeed={0.25} />
+        <OrbitControls makeDefault enabled={enableOrbitControls} autoRotate={autoRotate} autoRotateSpeed={0.25} />
         {/* <directionalLight castShadow position={[1, 2, 3]} intensity={1.5} />
         <ambientLight intensity={0.5} /> */}
         <color attach="background" args={['#171720']} />
@@ -47,8 +55,9 @@ export default function Scene() {
             {/* </Debug> */}
             <CrystallBall position={[8,4,0]}/>
         </Physics>
-        {/* <EffectComposer>
-            <Bloom luminanceThreshold={1} intensity={10} levels={9} mipmapBlur />
-        </EffectComposer> */}
+        <EffectComposer>
+            {DOFswitch ? <DepthOfField focusDistance={focusDistance} focalLength={focalLength} bokehScale={bokehScale} height={height} /> : <></>}
+            {/* <Bloom luminanceThreshold={1} intensity={10} levels={9} mipmapBlur /> */}
+        </EffectComposer>
     </>
 }
